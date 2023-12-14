84717bc983999f73cfaf26b2f238e6c4d54c989c
Tan Yiming <tanyiming.nus@gmail.com>
2023-12-14 21:03:09 +0800

    Style the ProjectCard component

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,12 +1,17 @@
 import Card from "./Card";
+import './ProjectCard.css';
 
 function ProjectCard({ project }) {
   return (
-    <Card>
-      <h2>{project.name}</h2>
-      <h3>{project.type}</h3>
-      <p>{project.description}</p>
-    </Card>
+    <div className='project-card'>
+      <Card>
+        <div className='content'>
+          <h3>{project.name}</h3>
+          <h4>{project.type}</h4>
+          <p>{project.description}</p>
+        </div>
+      </Card>
+    </div>
   )
 }
 
--- src/pages/ProjectOverview.js
+++ src/pages/ProjectOverview.js
@@ -3,7 +3,6 @@ import ProjectCard from "../components/ProjectCard"
 function ProjectOverview({ projects }) {
   return (
     <div>
-      <h1>these are the projects I did.</h1>
       {projects.map((project) => (
         <ProjectCard project={project} />
       ))}
