5aa8bc05758149455617aa8412d705efd22e08d5
Tan Yiming <tanyiming.nus@gmail.com>
2023-12-14 16:26:32 +0800

    Add state for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 function Home() {
+  const selfIntro = "I'm Mr Catawampus, a web developer from the UK. Click again to see my contact details.";
+  const contact = "My github: https://github.com/MrCatawampus"
+  
+  const [isContact, setIsContact] = useState(false);
+  const handleClick = () => setIsContact(!isContact);
+  const displayText = isContact ? contact : selfIntro;
   return (
     <div>
-      <h1>Home</h1>
-      <p>This is the home page</p>
+      <h1>Home Page</h1>
+      <h2 onClick={handleClick}>{displayText}</h2>
     </div>
   )
 }
